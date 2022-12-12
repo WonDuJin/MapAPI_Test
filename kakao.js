d3ba9a5ea374c58aac3e3a1a9379f568ff154f24
WonDuJin <windyjan1@naver.com>
2022-12-12 16:27:45 +0900

    API테스트

---
+++ kakao.js
@@ -0,0 +1,62 @@
+const infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };  
+
+let map = new kakao.maps.Map(mapContainer, mapOption);
+
+let ps = new kakao.maps.services.Places(); 	
+
+ps.keywordSearch("남산타워",placeesSearchCB);
+
+function placeesSearchCB (data, status, pagination){
+	if (status === kakao.maps.services.Status.OK){
+
+		let bounds = new kakao.maps.LatLngBounds();
+
+		for(let i = 0; i < data.length; i++ ){
+			displayMaker(data[i]);
+			bounds.extend(new kakao.maps.LatLng(data[i].y,data[i].x));
+		}
+		map.setBounds(bounds)
+	}
+}
+
+function displayMaker(place){
+
+	let maker = new kakao.maps.Marker({
+		map : map,
+		position : new kakao.maps.LatLng(place.y,place.x)
+	});
+
+	kakao.maps.event.addListener(maker,'click', function(){
+		infowindow.setContent('<div style="padding:5px; font-size:12px;">'+place.place_name + '</div>');
+		infowindow.open(map,maker);
+	})
+}
+
+
+// const maker  = new kakao.maps.Marker({
+
+// 	position : map.getCenter()
+// });
+
+// maker.setMap(map);
+
+
+// kakao.maps.event.addListener(map,'click',function(mouseEvent){
+
+// 	let latlng = mouseEvent.latLng; 
+
+// 	maker.setPosition(latlng);
+
+// 	let message = "클릭한 위치의 위도는 " + latlng.getLat() + "이고, " + '경도는' + latlng.getLng() + " 입니다.";
+			
+// 	console.log(message)
+
+// 	let resultDiv = document.getElementById("clickLatlng");
+// 	resultDiv.innerHTML	= message
+// });
