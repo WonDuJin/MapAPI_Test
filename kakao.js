bc38b82b98e3cf458b447aaa0156d7e4f35bb10f
WonDuJin <windyjan1@naver.com>
2022-12-12 18:07:45 +0900

    APi 테스트

--- kakao.js
+++ kakao.js
@@ -1,3 +1,16 @@
+// const roadviewContainer = document.getElementById("map");
+// const roadview = new kakao.maps.Roadview(roadviewContainer);
+// const roadviewClient = new kakao.maps.RoadviewClient();
+
+
+// const position = new kakao.maps.LatLng(37.5642029018226,126.98226365941522);
+
+// roadviewClient.getNearestPanoId(position, 50, function(panoId){
+// 	roadview.setPanoId(panoId, position);
+// });
+
+
+// 검색했을때 위치 출력 
 const infowindow = new kakao.maps.InfoWindow({zIndex:1});
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
@@ -10,7 +23,7 @@ let map = new kakao.maps.Map(mapContainer, mapOption);
 
 let ps = new kakao.maps.services.Places(); 	
 
-ps.keywordSearch("남산타워",placeesSearchCB);
+ps.keywordSearch("대전그린컴퓨터아트학원",placeesSearchCB);
 
 function placeesSearchCB (data, status, pagination){
 	if (status === kakao.maps.services.Status.OK){
@@ -24,7 +37,7 @@ function placeesSearchCB (data, status, pagination){
 		map.setBounds(bounds)
 	}
 }
-
+//마커 표시
 function displayMaker(place){
 
 	let maker = new kakao.maps.Marker({
@@ -39,24 +52,24 @@ function displayMaker(place){
 }
 
 
-// const maker  = new kakao.maps.Marker({
+const maker  = new kakao.maps.Marker({
 
-// 	position : map.getCenter()
-// });
+	position : map.getCenter()
+});
 
-// maker.setMap(map);
+maker.setMap(map);
 
 
-// kakao.maps.event.addListener(map,'click',function(mouseEvent){
+kakao.maps.event.addListener(map,'click',function(mouseEvent){
 
-// 	let latlng = mouseEvent.latLng; 
+	let latlng = mouseEvent.latLng; 
 
-// 	maker.setPosition(latlng);
+	maker.setPosition(latlng);
 
-// 	let message = "클릭한 위치의 위도는 " + latlng.getLat() + "이고, " + '경도는' + latlng.getLng() + " 입니다.";
+	let message = "클릭한 위치의 위도는 " + latlng.getLat() + "이고, " + '경도는' + latlng.getLng() + " 입니다.";
 			
-// 	console.log(message)
+	console.log(message)
 
-// 	let resultDiv = document.getElementById("clickLatlng");
-// 	resultDiv.innerHTML	= message
-// });
+	let resultDiv = document.getElementById("clickLatlng");
+	resultDiv.innerHTML	= message
+});
